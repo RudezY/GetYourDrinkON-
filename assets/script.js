70ff8fc7ba9924a6ad3488b302e71c6f9bd6561f
Rj Saini <rsaini88@gmail.com>
2022-11-03 22:04:07 -0700

    added HTML HTML anchor with some CSS

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,19 @@
-var apiIng = "www.thecocktaildb.com/api/json/v1/1/filter.php?i=" ;
-var apiDrink = "www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" ;
+var apiIng = "www.thecocktaildb.com/api/json/v1/1/filter.php?i=";
+var apiDrink = "www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+var formEl = $("#searchForm");
+var inputEl = $("#termInput");
+
+formEl.on("submit", function (e) {
+  e.preventDefault();
+  console.log("input", inputEl.val());
+  //now we take value and search api
+});
+
+function search(term) {
+  var url = "some url" + term;
+
+
+  //fetch
+  
+}
