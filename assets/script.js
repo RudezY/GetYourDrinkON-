c8047929be64aae33119d748068775d66ab0cde8
RudezY <rudyyamashiro@yahoo.com>
2022-11-08 00:40:02 -0800

    trying to get id for each drink to pull individual drink

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var formEl = $("#searchForm");
 var inputEl = $("#termInput");
 var drinkDisplay = $(".drinkDisplay")
 var resetbutton = $("#resetbtn");
+var selectedDrinkID = $("alldrinks");
 
 formEl.on("submit", function (e) {
   
@@ -20,9 +21,12 @@ formEl.on("submit", function (e) {
     
   }).then(data => {
     console.log(data);
+    document.querySelector('.drinkDisplay').innerHTML="";
     for (var i=0; i<data.drinks.length; i++) {
-console.log(data.drinks[i]);
-      var drinkDiv = document.createElement("div")
+  console.log(data.drinks[i]);
+      var drinkDiv = document.createElement("div");
+      drinkDiv.setAttribute("drink-id", data.drinks[i].idDrink)
+      drinkDiv.setAttribute("class", "alldrinks"+ [i])
       drinkDiv.innerHTML += `
     	<div style="vertical-align: middle;">
       	<img style="flex-grow: 1; height: 60px;" src="${data.drinks[i].strDrinkThumb}" />
@@ -31,16 +35,54 @@ console.log(data.drinks[i]);
     `
     document.querySelector('.drinkDisplay').appendChild(drinkDiv)
     drinkDiv.addEventListener('click', function(e) {
-
+      console.log(this.getAttribute("drink-id"))
+      var drinkIDS = (this.getAttribute("drink-id"));
+      localStorage.getItem("drink-id")
+      var oldIDS = localStorage.getItem("drink-id");
+      if (oldIDS) {
+        oldIDS = JSON.parse(oldIDS)
+      }else {
+        oldIDS = [];
+      }
+      oldIDS.push(drinkIDS)
+      localStorage.setItem('drink-ids',JSON.stringify(oldIDS));
+      // on click use apiDrink + drink-id to display drink with ingredients and instructions 
+    
     })
 
     }
 
   }).catch(err => console.log(err));
+
+
 })
+
+selectedDrinkID.on("click", (e) => {
+var drinkToShow = selectedDrinkID.val()
+var url2 = apiDrink + drinkToShow
+console.log(url2)
+fetch(`${url2}`)
+.then(response => {
+  return response.json()
+  
+}).then(data => {
+  console.log(data);
+
+
+
+
+
+
+});
+
+});
+
+
+
 
 function clearInput() {
   document.getElementById("searchForm").reset();
-  window.location.reload();
-}
+  document.querySelector('.drinkDisplay').innerHTML="";
+};
+
 $("#resetbtn").on("click", clearInput);
